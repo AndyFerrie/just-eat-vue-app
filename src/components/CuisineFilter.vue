5104af17da222ea92dcf8ddb4b1c24adf9c67620
Andy Ferrie <andy.ferrie@gmail.com>
2025-04-06 22:56:49 +0100

    Add CuisineFilter component

---
+++ src/components/CuisineFilter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex flex-wrap gap-2 mt-4">
+        <Button
+            :primary="selected === null"
+            :disabled="disabled"
+            @click="$emit('select', null)"
+        >
+            All
+        </Button>
+
+        <Button
+            v-for="cuisine in cuisines"
+            :key="cuisine"
+            :primary="selected === cuisine"
+            :disabled="disabled"
+            @click="$emit('select', cuisine)"
+        >
+            {{ cuisine }}
+        </Button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Button from '@/components/Button.vue'
+
+const { cuisines, selected, disabled } = defineProps<{
+  cuisines: string[]
+  selected: string | null
+  disabled?: boolean
+}>()
+
+defineEmits<{
+  (e: 'select', cuisine: string | null): void
+}>()
+</script>
