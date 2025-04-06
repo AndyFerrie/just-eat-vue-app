a454a6f6474b4facf5d8374c7a1a2d3eb430e102
Andy Ferrie <andy.ferrie@gmail.com>
2025-04-06 21:09:09 +0100

    Adds input component

---
+++ src/components/Input.vue
@@ -0,0 +1,27 @@
+<template>
+    <input
+        :type="type"
+        :placeholder="placeholder"
+        :value="modelValue"
+        :aria-label="ariaLabel"
+        @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+        :class="[
+      'px-6 py-4 text-base focus:outline-none rounded-full w-full',
+      extraClass,
+    ]"
+    />
+</template>
+
+<script setup lang="ts">
+const { modelValue, placeholder, type, extraClass, ariaLabel } = defineProps<{
+  modelValue: string
+  placeholder?: string
+  type?: string
+  extraClass?: string
+  ariaLabel?: string
+}>()
+
+defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+</script>
