c3141a42141c5a88863ca3ad0462645e46df9a2c
Andy Ferrie <andy.ferrie@gmail.com>
2025-04-06 18:26:44 +0100

    Add JET colours to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    content: ["./index.html", "./src/**/*.{vue, js, ts, jsx, tsx}"],
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
         extend: {
+            colors: {
+                primary: "#f36805", // Just Eat button color
+                brand: "#ff8000", // Just Eat background orange
+            },
             fontFamily: {
                 jetsans: ["JetSansDigital", "sans-serif"],
             },
