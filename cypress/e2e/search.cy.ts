2c0570dac62751f9b37c817170960d61c9574bb3
Andy Ferrie <andy.ferrie@gmail.com>
2025-04-07 13:07:51 +0100

    test: Add initial e2e Cypress tests

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,53 @@
+/// <reference types="cypress" />
+
+describe("Just Eat Search Page", () => {
+    beforeEach(() => {
+        cy.intercept("GET", "/api/restaurants/bypostcode/EC4M", {
+            fixture: "ec4m.json",
+        }).as("getRestaurants")
+
+        cy.visit("/")
+        cy.get('input[aria-label="Enter your postcode"]').type("EC4M")
+        cy.get('button[aria-label="Search by postcode"]').click()
+        cy.wait("@getRestaurants")
+    })
+
+    it("shows search results for EC4M", () => {
+        cy.url().should("include", "/search?postcode=EC4M")
+        cy.get('[data-testid="spinner"]').should("not.exist")
+
+        cy.contains(/^1995 restaurants deliver to EC4M$/).should("exist")
+        cy.get('[data-testid="restaurant-list"]')
+            .find('[role="listitem"]')
+            .should("have.length.greaterThan", 0)
+    })
+
+    it("filters restaurants by Authentic Pizza", () => {
+        cy.get('[data-testid="spinner"]').should("not.exist")
+        cy.contains("Authentic Pizza").click()
+        cy.contains(/^1 Authentic Pizza restaurant delivers to EC4M$/).should(
+            "exist"
+        )
+    })
+
+    it("filters restaurants by Burgers", () => {
+        cy.get('[data-testid="spinner"]').should("not.exist")
+        cy.contains("Burgers").click()
+        cy.contains(/^252 Burgers restaurants deliver to EC4M$/).should("exist")
+    })
+
+    it("shows message when no restaurants are found", () => {
+        cy.intercept("GET", "**/restaurants/bypostcode/NORESTAURANTS", {
+            MetaData: { CuisineDetails: [] },
+            Restaurants: [],
+        }).as("getEmptyResults")
+
+        cy.visit("/")
+        cy.get('input[aria-label="Enter your postcode"]').type("NORESTAURANTS")
+        cy.get('button[aria-label="Search by postcode"]').click()
+        cy.wait("@getEmptyResults")
+
+        cy.get('[data-testid="spinner"]').should("not.exist")
+        cy.contains("Hm, we couldn’t find 'NORESTAURANTS'").should("exist")
+    })
+})
