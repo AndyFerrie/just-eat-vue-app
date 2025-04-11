0a1672fe951516549a1419d01b2577c1975a4f0b
Andy Ferrie <andy.ferrie@gmail.com>
2025-04-11 08:11:48 +0100

    Add CuisineDetail to transform util

--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -1,11 +1,11 @@
-import type { Restaurant } from "@/types/restaurants"
+import type { Restaurant, CuisineDetail } from "@/types/restaurants"
 
 export const transformRestaurant = (restaurant: any): Restaurant => ({
     id: restaurant.Id,
     name: restaurant.Name,
     logoUrl: restaurant.LogoUrl,
     rating: { starRating: restaurant.Rating.StarRating },
-    cuisines: restaurant.Cuisines.map((cuisine: any) => ({
+    cuisines: restaurant.Cuisines.map((cuisine: CuisineDetail) => ({
         name: cuisine.Name,
     })),
 })
